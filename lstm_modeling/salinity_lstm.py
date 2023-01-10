ed9b94c95eb9cf76acc0cea663a140dd6f2eac4e
Lindsay Platt <lrplatt@wisc.edu>
2023-01-10 11:11:20 -0600

    setup basic lstm fxns from wcneill example

---
+++ lstm_modeling/salinity_lstm.py
@@ -0,0 +1,75 @@
+# LSTM Modeling Functions for Salinity
+
+# References:
+#     - https://github.com/wcneill/jn-ml-textbook/blob/master/Deep%20Learning/04%20Recurrent%20Networks/pytorch13b_LSTM.ipynb
+
+##### Define the LSTM class & functions #####
+
+class salinityLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc   = nn.Linear(hidden_size, output_size)
+        
+    def forward(self, x, hs):
+   
+        out, hs = self.lstm(x, hs)           # out.shape = (batch_size, seq_len, hidden_size)
+        out = out.view(-1, self.hidden_size) # out.shape = (seq_len, hidden_size)     
+        out = self.fc(out)
+        
+        return out, hs
+
+##### Define the function that will train the model #####
+
+def train(model, epochs, train_set, valid_data=None, lr=0.001, print_every=100):
+
+    criterion = nn.MSELoss()
+    opt = optim.Adam(model.parameters(), lr=lr)
+
+    train_loss = []
+    valid_loss = []
+
+    for e in range(epochs):
+
+        hs = None
+        t_loss = 0
+        for x, y in get_batches(train_set, 12):
+
+            opt.zero_grad()
+
+            # Create batch_size dimension
+            x = x.unsqueeze(0)
+            out, hs = model(x, hs)
+            hs = tuple([h.data for h in hs])
+
+            loss = criterion(out, y)
+            loss.backward()
+            opt.step()
+            t_loss += loss.item()
+
+        if valid_data is not None:
+                model.eval()
+                val_x, val_y = valid_data
+                val_x = val_x.unsqueeze(0)
+                preds, _ = model(val_x, hs)
+                v_loss = criterion(preds, val_y)
+                valid_loss.append(v_loss.item())
+
+                model.train()
+
+        train_loss.append(np.mean(t_loss))
+
+
+        if e % print_every == 0:
+            print(f'Epoch {e}:\nTraining Loss: {train_loss[-1]}')
+            if valid_data is not None:
+                print(f'Validation Loss: {valid_loss[-1]}')
+
+    plt.figure(figsize=[8., 6.])
+    plt.plot(train_loss, label='Training Loss')
+    plt.plot(valid_loss, label='Validation Loss')
+    plt.title('Loss vs Epochs')
+    plt.xlabel('Epochs')
+    plt.show()
+
