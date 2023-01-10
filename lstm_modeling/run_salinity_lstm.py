0d5391e216406f0788ea336a22b5e9beb5f42f62
Lindsay Platt <lrplatt@wisc.edu>
2023-01-10 11:43:54 -0600

    add wcneill example code for training the model

--- lstm_modeling/run_salinity_lstm.py
+++ lstm_modeling/run_salinity_lstm.py
@@ -15,3 +15,38 @@ import seaborn as sns
 # Load local functions
 import salinity_lstm
 
+##### Load and process the data #####
+
+data = sns.load_dataset("flights")
+data.passengers = data.passengers.astype(np.float32) # Convert passenger data to float for PyTorch
+
+# Scale/transform data
+scaler = MinMaxScaler(feature_range = -1,1)
+data = scaler.fit_transform(data)
+
+# Split data into test and training
+
+train_data = data.passengers[:-12].to_numpy().reshape(-1,1)
+valid_data = data.passengers[-12:].to_numpy().reshape(-1,1)
+
+# Convert to a tensor
+train_data = torch.tensor(train_data, dtype=torch.float32)
+valid_data = torch.tensor(valid_data, dtype=torch.float32)
+
+##### Define model and its parameters #####
+
+input_size = 1
+hidden_size = 100
+num_layers = 1
+output_size = 1
+
+lstm_model = salinityLSTM(input_size, hidden_size, num_layers, output_size)
+
+##### Train the model #####
+
+num_epochs = 500
+learning_rate = 0.0005
+
+train(lstm_model, num_epochs, train_data, valid_data, lr=learning_rate)
+
+
