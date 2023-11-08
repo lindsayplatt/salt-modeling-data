5457e89218ecb2b9a4195379b5ad151bf0425a36
Lindsay Platt <lrplatt@wisc.edu>
2023-11-08 14:57:25 -0600

    remove commented code

--- 2_Prepare/src/attr_combine_all.R
+++ 2_Prepare/src/attr_combine_all.R
@@ -20,11 +20,3 @@ combine_static_attributes <- function(...) {
       dplyr::left_join, 
       by = 'site_no')
 }
-
-# TODO: evaluate output of attributes. Might not be useful to use the ones
-# whose histograms aren't nicely distributed? Probably won't have much
-# explanatory power. Also, there are fewer unique ids than there are rows
-# in this dataset (match to site nums and see what is going on there) + check
-# on which comids are missing? or duplicated?
-# glimpse(tar_read(p2_attr_all))
-# skimr::skim(tar_read(p2_attr_all)) 
