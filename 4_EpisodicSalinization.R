1d19272f4f00f7c5f3a90319a8936bd5a66ca1b3
Lindsay Platt <lrplatt@wisc.edu>
2024-02-02 10:13:00 -0600

    cleanup targets and descriptions for 4_EpisodicSalinization

--- 4_EpisodicSalinization.R
+++ 4_EpisodicSalinization.R
@@ -13,22 +13,24 @@ p4b_targets <- list(
   
   # Calculate event peaks for all sites 
   tar_target(p4_ts_sc_peaks, {
-      # filter(site_no %in% c('04208000', '01095220', '01645704', '01645762')) %>% 
     p4b_ts_sc_norm %>% 
       split(.$site_no) %>%
       map(~find_event_peaks(ts_data = .x,
                             date_colname = 'dateTime',
                             param_colname = 'SpecCond_norm',
+                            # TODO: We could look at adjusting what makes it a "peak"
                             sb_pk_thresh = 0.000005,
-                            sf_pk_thresh = 0.1) # Switch to a different number?
-      ) %>%
-      bind_rows()
+                            sf_pk_thresh = 0.1) 
+      ) %>% bind_rows()
   }),
   
-  tar_target(p4_ts_sc_peak_summary, summarize_salt_peaks(p4_ts_sc_peaks, min_winter_perc = 0.40))
+  # Now summarize the peak information and filter to just those sites that meet 
+  # our criteria for exhibiting "episodic" patterns in winter.
   tar_target(p4_ts_sc_peak_summary, 
              summarize_salt_peaks(p4_ts_sc_peaks, min_winter_perc = 0.40, min_perc_diff = 0.10)),
   tar_target(p4_episodic_sites, filter(p4_ts_sc_peak_summary, is_salt_site)$site_no)
+  
+  # TODO: revisit three that are still questionable - c('02042500', '04024000', '04067500')
   
   # Hilary's original list:
   # tar_target(p4b_episodic_sites, c("01095434", "01104415", "01104420", "01104455", "01104460", 
