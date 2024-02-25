9e356c37bd97212bbb6714bcdc84c5894a8b217f
Lindsay Platt <lrplatt@wisc.edu>
2024-02-25 17:00:28 -0600

    build with an additional year of data

--- 1_Download.R
+++ 1_Download.R
@@ -19,7 +19,7 @@ p1_targets <- list(
   ###### NWIS DATA 0: Set download configs for streamflow (Q) & specific conductivity (SC) ######
   
   tar_target(p1_nwis_start_date, as.Date('1950-01-01')), 
-  tar_target(p1_nwis_end_date, as.Date('2022-12-31')),
+  tar_target(p1_nwis_end_date, as.Date('2023-12-31')), 
   tar_target(p1_nwis_pcode_sc, '00095'), # NWIS specific conductance code
   tar_target(p1_nwis_pcode_q, '00060'), # NWIS streamflow code
   tar_target(p1_nwis_min_years, 10), # Minimum number of years required
