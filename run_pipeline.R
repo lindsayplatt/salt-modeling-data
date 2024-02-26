ef6a137bc5a9018e1cf7297e379aa6e2917c3fbc
Lindsay Platt <lrplatt@wisc.edu>
2024-02-25 19:58:20 -0600

    remove 0_Explore directory

--- run_pipeline.R
+++ run_pipeline.R
@@ -1,12 +1,4 @@
 # Use this to start a background job in RStudio that will run the full pipeline
 
-# RUN ORIGINAL, PRE-PROPOSAL CODE
-# wd <- getwd()
-# setwd('0_Explore')
-# library(targets)
-# tar_make()
-# setwd(wd)
-
-# RUN CURRENT PIPELINE, POST-PROPOSAL
 library(targets)
 tar_make()
