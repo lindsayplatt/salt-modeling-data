b30f7341ed5aea3be7dcfc81c0f6e6253dd4918d
Lindsay Platt <lrplatt@wisc.edu>
2024-03-26 19:06:09 -0500

    add polished version of partial dependence plots (with better attribute display names!)

--- 7_Disseminate.R
+++ 7_Disseminate.R
@@ -1,8 +1,30 @@
 # Targets for creating paper and slidedeck visualizations
 
 source('7_Disseminate/src/roadSalt_figs.R')
+source('7_Disseminate/src/partialDependence_figs.R')
 
 p7_targets <- list(
+  
+  # Create a manual crosswalk between attribute names and display names
+  tar_target(p7_attr_name_xwalk, tibble(
+    attribute = c("medianFlow", "roadSaltPerSqKm", "annualPrecip", "baseFlowInd", 
+                  "subsurfaceContact", "gwRecharge", "pctOpenWater", "basinSlope", 
+                  "pctForested", "pctWetland", "pctAgriculture", "pctDeveloped", 
+                  "annualSnow", "winterAirTemp", "depthToWT", "transmissivity"),
+    display_name = c("Median Flow (m3/s)", "Road Salt / SqKm (kg)", "Precip (mm/yr)", "Baseflow Index",
+                     "Subsurface Contact (days)", "GW Recharge (mm/yr)", "Open Water (%)", "Basin Slope (%)",
+                     "Forested (% area)", "Wetland (% area)", "Agriculture (% area)", "Developed (% area)", 
+                     "Snow (mm/yr)", "Winter Air Temp (°C)", "Depth to WT (m)", "Transmissivity (m2/day)"))),
+  
+  # Isolate overall importance and out-of-bag errors
+  tar_target(p7_overall_attr_importance_episodic, p6b_rf_attr_importance %>% 
+               filter(site_category == 'Overall mean') %>% 
+               arrange(desc(importance))),
+  tar_target(p7_oob_error_episodic, p6b_rf_oob),
+  tar_target(p7_overall_attr_importance_baseflow, p6c_rf_attr_importance %>% 
+               filter(site_category == 'Overall mean') %>% 
+               arrange(desc(importance))),
+  tar_target(p7_oob_error_baseflow, p6c_rf_oob),
   
   # Create a single dataset that collapses all categorization data of baseflow
   # and episodic into a single data.frame (so each site should appear twice)
@@ -22,6 +44,20 @@ p7_targets <- list(
   # Map of roadsalt per site showing that sites don't completely follow a gradient from south --> north
   tar_target(p7_roadsalt_sitemap_png, 
              create_roadSalt_site_map('7_Disseminate/out/roadSalt_sitemap.png', p3_static_attributes, p1_nwis_sc_sites_sf, p1_conus_state_cds),
+             format = 'file'),
+  
+  # Partial dependence plots showing how probability varies by attribute value
+  tar_target(p7_partDep_episodic_png, 
+             create_partialDependence_miniPlots_figure('7_Disseminate/out/partDep_episodic.png', 
+                                                       p6b_rf_attr_partdep, p6b_site_attr_rf_optimal,
+                                                       p7_overall_attr_importance_episodic$attribute,
+                                                       p7_attr_name_xwalk, '#0b5394'),
+             format = 'file'),
+  tar_target(p7_partDep_baseflow_png, 
+             create_partialDependence_miniPlots_figure('7_Disseminate/out/partDep_baseflow.png', 
+                                                       p6c_rf_attr_partdep, p6c_site_attr_rf_optimal,
+                                                       p7_overall_attr_importance_baseflow$attribute,
+                                                       p7_attr_name_xwalk, '#b45f06'),
              format = 'file')
   
 )
